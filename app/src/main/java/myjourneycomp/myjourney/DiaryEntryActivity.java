6c9ef2e42f8f705a0dd7fed9d232f98f82ddf6c9
pownyhof <andreas1.weber@stud.uni-regensburg.de>
2018-08-02 17:05:09 +0200

    new classes added

---
+++ app/src/main/java/myjourneycomp/myjourney/DiaryEntryActivity.java
@@ -0,0 +1,12 @@
+package myjourneycomp.myjourney;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DiaryEntryActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diaryentry);
+    }
+}
---
+++ app/src/main/java/myjourneycomp/myjourney/DiaryEntryListActivity.java
@@ -0,0 +1,13 @@
+package myjourneycomp.myjourney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryEntryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_entry_list);
+    }
+}
